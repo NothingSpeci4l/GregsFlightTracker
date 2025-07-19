ae8a084bc237078d19bafae8031d78331f2afdbd
Grégoire Wild <115501088+NothingSpeci4l@users.noreply.github.com>
2025-07-19 18:17:32 +0200

    Settings Section

--- src/main/java/com/gregwll/flighttracker/Main.java
+++ src/main/java/com/gregwll/flighttracker/Main.java
@@ -3,8 +3,11 @@ package com.gregwll.flighttracker;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.*;
@@ -12,9 +15,17 @@ import javafx.scene.paint.Color;
 import javafx.scene.web.WebView;
 import javafx.stage.Stage;
 
+import javax.xml.stream.Location;
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
 import java.util.Objects;
 
 public class Main extends Application {
+
+    private static Stage stage2;
+    private static Scene mainscene;
+    private static Scene settingscene;
 
     @Override
     public void start(Stage stage) {
@@ -44,13 +55,31 @@ public class Main extends Application {
 
 
         settingsBtn.setOnAction(new EventHandler<ActionEvent>() {
+
+
             @Override
             public void handle(ActionEvent actionEvent) {
+               var url = getClass().getResource("/settings.fxml");
+                if (url == null) {
+                    System.err.println("settings.fxml introuvable !");
+                    System.exit(1);
+                }
+                Parent root;
                 try {
-                    SettingsWindow.openSettings(getClass().getResource("settings.fxml"));
-                } catch (Exception e) {
+                    root = FXMLLoader.load(url);
+                } catch (IOException e) {
                     throw new RuntimeException(e);
                 }
+                Scene scene = new Scene(root, mainscene.getWidth(), mainscene.getHeight());
+                var urlcss = getClass().getResource("/style.css");
+                if (urlcss == null) {
+                    System.err.println("stylecss introuvable !");
+                    System.exit(1);
+                }
+                scene.getStylesheets().add(urlcss.toExternalForm());
+                settingscene = scene;
+                stage.setScene(scene);
+                stage.show();
             }
         });
 
@@ -76,9 +105,20 @@ public class Main extends Application {
         stage.setScene(scene);
         stage.setTitle("Gregs Flight Tracker - V-dev0.1");
         stage.show();
+
+        stage2 = stage;
+        mainscene = scene;
+
     }
 
     public static void main(String[] args) {
         launch();
+
     }
+
+    public static void show() {
+        stage2.setScene(mainscene);
+    }
+
+
 }
---
+++ src/main/java/com/gregwll/flighttracker/SettingsController.java
@@ -0,0 +1,10 @@
+package com.gregwll.flighttracker;
+
+import javafx.event.ActionEvent;
+
+public class SettingsController {
+
+    public void saveClose(ActionEvent e) {
+        Main.show();
+    }
+}
